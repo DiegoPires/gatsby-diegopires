af251ea52b099fac91507671c7574681ee4e45dc
Diego Silva Pires <diego_silva_pires@outlook.com>
2018-07-03 20:25:06 -0400

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Diego Pires</h1>
+    <p>Analyst développeur, pseudo-dessinateur, photographe en herbe, athlète de fin de semaine, polyglotte et troglodyte.</p>
+  </div>
+)
+
+export default IndexPage
